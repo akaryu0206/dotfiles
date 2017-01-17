8ce3ce0352a2b9eb98420fc5e821fb8a252d7c9d
Ryuji Akamatsu <r-akamatsu@sneaz.co.jp>
2017-01-17 09:41:08 +0900

    update

--- linkDotfiles.sh
+++ linkDotfiles.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
+
 ln -sf ~/dotfiles/.bash_profile ~/.bash_profile
 ln -sf ~/dotfiles/.vimrc ~/.vimrc
 ln -sf ~/dotfiles/.gitignore_global ~/.gitignore_global
 ln -sf ~/dotfiles/.zshrc ~/.zshrc
 ln -sf ~/dotfiles/.zshenv ~/.zshenv
+
+exec $SHELL -l
