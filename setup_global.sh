387c05fdb630553738d9de3c5c6bd179567e2c9a
Ryuji Akamatsu <r-akamatsu@sneaz.co.jp>
2017-03-08 10:31:55 +0900

    remove .anyenv

--- setup_global.sh
+++ setup_global.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 
-ln -sf ~/dotfiles/.anyenv ~/.anyenv
 ln -sf ~/dotfiles/.bash_profile ~/.bash_profile
 ln -sf ~/dotfiles/.vimrc ~/.vimrc
 ln -sf ~/dotfiles/.gitignore_global ~/.gitignore_global
