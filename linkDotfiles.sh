a11b456801a50e65a26b0a09d18ae89c42b432fc
Ryuji Akamatsu <r-akamatsu@sneaz.co.jp>
2017-03-08 10:09:03 +0900

    add .anyenv without envs

--- linkDotfiles.sh
+++ linkDotfiles.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+ln -sf ~/dotfiles/.anyenv ~/.anyenv
 ln -sf ~/dotfiles/.bash_profile ~/.bash_profile
 ln -sf ~/dotfiles/.vimrc ~/.vimrc
 ln -sf ~/dotfiles/.gitignore_global ~/.gitignore_global
