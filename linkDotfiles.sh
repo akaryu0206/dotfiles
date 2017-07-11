129bf8dcd1ee3da5a107f2044f3c12fddd33a1fb
Ryuji Akamatsu <seven.television.commercials@gmail.com>
2017-07-11 21:09:06 +0900

    delete zsh files.

--- linkDotfiles.sh
+++ linkDotfiles.sh
@@ -3,7 +3,5 @@
 ln -sf ~/dotfiles/.bash_profile ~/.bash_profile
 ln -sf ~/dotfiles/.vimrc ~/.vimrc
 ln -sf ~/dotfiles/.gitignore_global ~/.gitignore_global
-ln -sf ~/dotfiles/.zshrc ~/.zshrc
-ln -sf ~/dotfiles/.zshenv ~/.zshenv
 
 exec $SHELL -l
