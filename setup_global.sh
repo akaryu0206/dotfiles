6236372438017eeadf09a9f251890dadf9e0a8ff
Ryuji Akamatsu <seven.television.commercials@gmail.com>
2017-08-29 11:43:22 +0900

    modify

--- setup_global.sh
+++ setup_global.sh
@@ -1,9 +1,55 @@
 #!/bin/sh
 
-ln -sf ~/dotfiles/.zshrc ~/.zshrc
-ln -sf ~/dotfiles/.zshenv ~/.zshenv
+# install zplug
+
+if [ ! -d ~/.zplug ]; then
+  curl -sL --proto-redir -all,https https://raw.githubusercontent.com/zplug/installer/master/installer.zsh| zsh
+fi
+
+# install anyenv
+
+if [ ! -d ~/.anyenv ]; then
+  git clone https://github.com/riywo/anyenv ~/.anyenv
+  export PATH="$HOME/.anyenv/bin:$PATH"
+  eval "$(anyenv init -)"
+fi
+
+# install rbenv
+
+if [ ! -d ~/.anyenv/envs/rbenv ]; then
+  anyenv install rbenv
+fi
+
+# install pyenv
+
+if [ ! -d ~/.anyenv/envs/pyenv ]; then
+  anyenv install pyenv
+fi
+
+# install phpenv
+
+if [ ! -d ~/.anyenv/envs/phpenv ]; then
+  anyenv install phpenv
+fi
+
+# install ndenv
+
+if [ ! -d ~/.anyenv/envs/ndenv ]; then
+  anyenv install ndenv
+fi
+
+# link dotfiles
+
 ln -sf ~/dotfiles/.gitignore_global ~/.gitignore_global
-ln -sf ~/dotfiles/.vimrc ~/.vimrc
 ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
+ln -sf ~/dotfiles/.vimrc ~/.vimrc
+ln -sf ~/dotfiles/.zshenv ~/.zshenv
+ln -sf ~/dotfiles/.zshrc ~/.zshrc
+
+# install zsh plugins via zplug
+
+zplug install
+
+# restart shell
 
 exec $SHELL -l
