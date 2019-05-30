f85741bffcdaa4b2088c470d600d3103bc4d693b
Ryuji Akamatsu <seven.television.commercials@gmail.com>
2019-05-30 13:54:29 +0900

    update Hyper settings

--- .hyper.js
+++ .hyper.js
@@ -9,7 +9,7 @@ module.exports = {
     updateChannel: 'stable',
 
     // default font size in pixels for all tabs
-    fontSize: 18,
+    fontSize: 15,
 
     // font family with optional fallbacks
     fontFamily: 'Ricty Diminished, Menlo, "DejaVu Sans Mono", Consolas, "Lucida Console", monospace',
@@ -85,23 +85,23 @@ module.exports = {
       lightWhite: '#FFFFFF',
     },
 
-    MaterialTheme: {
-      // Set the theme variant,
-      // OPTIONS: 'Darker', 'Palenight', 'Ocean', ''
-      theme: 'Palenight',
-
-      // [Optional] Set the rgba() app background opacity, useful when enableVibrance is true
-      // OPTIONS: From 0.1 to 1
-      backgroundOpacity: '0.5',
-
-      // [Optional] Set the accent color for the current active tab
-      accentColor: '#64FFDA',
-
-      // [Optional] Mac Only. Need restart. Enable the vibrance and blurred background
-      // OPTIONS: 'dark', 'ultra-dark', 'bright'
-      // NOTE: The backgroundOpacity should be between 0.1 and 0.9 to see the effect.
-        vibrancy: 'dark'
-    },
+//    MaterialTheme: {
+//      // Set the theme variant,
+//      // OPTIONS: 'Darker', 'Palenight', 'Ocean', ''
+//      theme: 'Palenight',
+//
+//      // [Optional] Set the rgba() app background opacity, useful when enableVibrance is true
+//      // OPTIONS: From 0.1 to 1
+//      backgroundOpacity: '0.5',
+//
+//      // [Optional] Set the accent color for the current active tab
+//      accentColor: '#64FFDA',
+//
+//      // [Optional] Mac Only. Need restart. Enable the vibrance and blurred background
+//      // OPTIONS: 'dark', 'ultra-dark', 'bright'
+//      // NOTE: The backgroundOpacity should be between 0.1 and 0.9 to see the effect.
+//        vibrancy: 'dark'
+//    },
 
     // the shell to run when spawning a new session (i.e. /usr/local/bin/fish)
     // if left empty, your system's login shell will be used by default
@@ -156,7 +156,7 @@ module.exports = {
   //   `@company/project`
   //   `project#1.0.1`
   plugins: [
-    'hyper-material-theme',
+    //'hyper-material-theme',
     'hyperterm-1password',
     'hyperterm-paste',
     'hyperlinks'
